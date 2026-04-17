16beeb6260aa221a2f222f6b046e2019ee3f8d04
LaFaucheuse80 <lahcenmeziane02@gmail.com>
2026-04-17 09:37:47 +0200

    test

--- src/AiguilleMinute.java
+++
@@ -1,9 +0,0 @@
-public class AiguilleMinute extends Aiguille{
-    public AiguilleMinute(int _longueur, int centreX, int centreY){
-        super(_longueur, centreX, centreY);
-    }
-
-    public double calculAngle(Heure heure){
-        return Math.toRadians(heure.getMinutes() * 6 - 90);
-    }
-}
